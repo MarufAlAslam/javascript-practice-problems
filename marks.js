248c6c4c6dab83afa828335a7af4de2e1ae9a4fc
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-02 02:12:15 +0600

    score problem done

---
+++ marks.js
@@ -0,0 +1,91 @@
+var myScore = 85
+var tomScore = 66
+var janeScore = 95
+var petersScore = 56
+var johnScore = 40
+
+if (myScore >= 80) {
+    console.log("I've got A Grade")
+}
+else if (myScore >= 60) {
+    console.log("I've got B Grade")
+}
+else if (myScore >= 50) {
+    console.log("I've got C Grade")
+}
+else if (myScore >= 40) {
+    console.log("I've got D Grade")
+}
+else {
+    console.log("I've got F Grade")
+}
+
+
+if (tomScore >= 80) {
+    console.log("Tom Has got A Grade")
+}
+else if (tomScore >= 60) {
+    console.log("Tom Has got B Grade")
+}
+else if (tomScore >= 50) {
+    console.log("Tom Has got C Grade")
+}
+else if (tomScore >= 40) {
+    console.log("Tom Has got D Grade")
+}
+else {
+    console.log("Tom Has got F Grade")
+}
+
+
+
+if (janeScore >= 80) {
+    console.log("Jane Has got A Grade")
+}
+else if (janeScore >= 60) {
+    console.log("Jane Has got B Grade")
+}
+else if (janeScore >= 50) {
+    console.log("Jane Has got C Grade")
+}
+else if (janeScore >= 40) {
+    console.log("Jane Has got D Grade")
+}
+else {
+    console.log("Jane Has got F Grade")
+}
+
+
+
+if (petersScore >= 80) {
+    console.log("Peter Has got A Grade")
+}
+else if (petersScore >= 60) {
+    console.log("Peter Has got B Grade")
+}
+else if (petersScore >= 50) {
+    console.log("Peter Has got C Grade")
+}
+else if (petersScore >= 40) {
+    console.log("Peter Has got D Grade")
+}
+else {
+    console.log("Peter Has got F Grade")
+}
+
+
+if (johnScore >= 80) {
+    console.log("John Has got A Grade")
+}
+else if (johnScore >= 60) {
+    console.log("John Has got B Grade")
+}
+else if (johnScore >= 50) {
+    console.log("John Has got C Grade")
+}
+else if (johnScore >= 40) {
+    console.log("John Has got D Grade")
+}
+else {
+    console.log("John Has got F Grade")
+}
