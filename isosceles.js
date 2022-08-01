d35ab5a243bc0c5320aadf50234e47f9b4d12494
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-02 02:18:20 +0600

    isosceles triangle problem done

---
+++ isosceles.js
@@ -0,0 +1,7 @@
+var side1 = 9, side2 = 8, side3 = 9;
+if (side1 == side2 || side1 == side3 || side2 == side3) {
+    console.log("This is an isosceles triangle");
+}
+else {
+    console.log("This is not an isosceles triangle");
+}
