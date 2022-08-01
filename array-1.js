37a37384397f063cbe9dcb41cebce38e10864eef
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-02 01:55:24 +0600

    array problem done

---
+++ array-1.js
@@ -0,0 +1,17 @@
+var fruits = ["Banana", "Banana", "Orange"];
+
+
+// index of Banana
+console.log(fruits.indexOf("Banana"))
+
+// replacing Banana with Mango'
+fruits[0] = "Mango"
+console.log(fruits)
+
+// removing Orange
+fruits.pop()
+console.log(fruits)
+
+// adding Watermelon
+fruits.push("Watermelon")
+console.log(fruits)
