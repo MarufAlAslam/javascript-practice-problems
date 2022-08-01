130cf72b23dc5ffdc185b29c16ac2f1eea63b329
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-02 02:15:22 +0600

    largest number problem done

---
+++ largest-number.js
@@ -0,0 +1,10 @@
+var num1 = 13, num2 = 79, num3 = 45
+if (num1 > num2 && num1 > num3) {
+    console.log(num1 + " is the largest number")
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log(num2 + " is the largest number")
+}
+else {
+    console.log(num3 + " is the largest number")
+}
