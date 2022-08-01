c7aeeaa6ba124aaad05ee5ce4c4dc0d524a4bfdc
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-02 01:36:43 +0600

    2nd problem done

---
+++ averageScore.js
@@ -0,0 +1,7 @@
+var mathScore = 75.25;
+var biologyScore = 65;
+var chemistryScore = 80;
+var physicsScore = 35.45;
+var banglaScore = 99.50;
+var averageScore = (mathScore + biologyScore + chemistryScore + physicsScore + banglaScore) / 5;
+console.log(averageScore.toFixed(2))
